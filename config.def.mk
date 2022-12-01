85a334dd1fb6f06aaf5f461b45873c7f97c3834a
Paweł <pawel.anikiel@gmail.com>
2022-12-01 22:20:48 +0100

    fix windows networking

--- config.def.mk
+++ config.def.mk
@@ -1,7 +1,7 @@
-CXXFLAGS = -O2 -DNDEBUG
+CXXFLAGS = -O2 -DNDEBUG -I.
 
 # Windows:
-LDFLAGS = -lglfw3 -lgdi32 -lopengl32
+LDFLAGS = -lglfw3 -lgdi32 -lopengl32 -lws2_32
 TARGET  = main.exe
 
 # Linux:
--- net.cpp
+++ net.cpp
@@ -1,12 +1,21 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#define bswap_64(x) _byteswap_uint64(x)
+#else
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <byteswap.h>
+#endif
 
 #define PORT 8088
+#define ADDR "192.168.0.106"
  
 static int sock;
 
@@ -21,7 +30,7 @@ int minit(void)
 
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(PORT);
-	inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+	inet_pton(AF_INET, ADDR, &addr.sin_addr);
 
 	res = connect(sock, (struct sockaddr *)&addr, sizeof(addr));
 	if (res < 0) {
@@ -29,7 +38,7 @@ int minit(void)
 		return res;
 	}
 
-	write(sock, "AAAAAAAAAAAAAAAAAAAAAAA", 23);
+	send(sock, "AAAAAAAAAAAAAAAAAAAAAAA", 23, 0);
 
 	return 0;
 }
@@ -46,10 +55,10 @@ void mw(const char *name, int num_cnt, ...)
 	if (sock <= 0)
 		return;
 
-	write(sock, &who, 1);
-	write(sock, &name_len_n, 4);
-	write(sock, &num_cnt_n, 4);
-	write(sock, name, name_len);
+	send(sock, &who, 1, 0);
+	send(sock, (const char *)&name_len_n, 4, 0);
+	send(sock, (const char *)&num_cnt_n, 4, 0);
+	send(sock, name, name_len, 0);
 
 	va_start(va, num_cnt);
 	for (i = 0; i < num_cnt; i++) {
@@ -59,6 +68,6 @@ void mw(const char *name, int num_cnt, ...)
 		} num;
 		num.d = va_arg(va, double);
 		num.u = bswap_64(num.u);
-		write(sock, &num, 8);
+		send(sock, (const char *)&num, 8, 0);
 	}
 }
