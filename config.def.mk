ebadf3166636bad82d565879d240c362c045fe5e
Paweł <pawel.anikiel@gmail.com>
2023-03-17 12:46:54 +0100

    wasm config

--- config.def.mk
+++ config.def.mk
@@ -7,3 +7,11 @@ TARGET  = main.exe
 # Linux:
 #LDFLAGS = -lGL -lglfw
 #TARGET  = main
+
+# wasm:
+#LD = wasm-ld
+#LDFLAGS = --no-entry -export-all
+#CC = clang
+#CFLAGS = --target=wasm32 -nostdlib -I fcsim/stdlib -O2
+#CXX = clang++
+#CXXFLAGS = --target=wasm32 -nostdlib -fno-rtti -fno-exceptions -I fcsim/stdlib -O2
