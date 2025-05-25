f9d2b77a5f22823428d7dec9bf62c774efacd14c
Paweł Anikiel <Pawel Anikiel pawel.anikiel@gmail.com>
2025-05-26 00:23:15 +0200

    a

--- main.c
+++ main.c
@@ -1,167 +1,73 @@
-
-/* An example of the minimal Win32 & OpenGL program.  It only works in
-   16 bit color modes or higher (since it doesn't create a
-   palette). */
-
-
-#include <windows.h>			/* must include this before GL/gl.h */
-#include <GL/gl.h>			/* OpenGL header file */
-#include <GL/glu.h>			/* OpenGL utilities header file */
+#include <windows.h>
+#include <GL/gl.h>
 #include <stdio.h>
+#include <stdbool.h>
 
+bool running = true;
 
-void
-display()
-{
-    /* rotate a triangle around */
-    glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_TRIANGLES);
-    glColor3f(1.0f, 0.0f, 0.0f);
-    glVertex2i(0,  1);
-    glColor3f(0.0f, 1.0f, 0.0f);
-    glVertex2i(-1, -1);
-    glColor3f(0.0f, 0.0f, 1.0f);
-    glVertex2i(1, -1);
-    glEnd();
-    glFlush();
-}
-
-
-LONG WINAPI
-WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LONG WINAPI WindowProc(HWND window, UINT msg, WPARAM wparam, LPARAM lparam)
 { 
-    static PAINTSTRUCT ps;
-
-    switch(uMsg) {
-    case WM_PAINT:
-	display();
-	BeginPaint(hWnd, &ps);
-	EndPaint(hWnd, &ps);
-	return 0;
-
-    case WM_SIZE:
-	glViewport(0, 0, LOWORD(lParam), HIWORD(lParam));
-	PostMessage(hWnd, WM_PAINT, 0, 0);
-	return 0;
-
-    case WM_CHAR:
-	switch (wParam) {
-	case 27:			/* ESC key */
-	    PostQuitMessage(0);
-	    break;
+	switch(msg) {
+	case WM_CLOSE:
+		running = false;
+		return 0;
 	}
-	return 0;
 
-    case WM_CLOSE:
-	PostQuitMessage(0);
-	return 0;
-    }
-
-    return DefWindowProc(hWnd, uMsg, wParam, lParam); 
+	return DefWindowProc(window, msg, wparam, lparam);
 } 
 
-HWND
-CreateOpenGLWindow(char* title, int x, int y, int width, int height, 
-		   BYTE type, DWORD flags)
+int APIENTRY WinMain(HINSTANCE inst, HINSTANCE prev_inst, LPSTR cmd_line, int cmd_show)
 {
-    int         pf;
-    HDC         hDC;
-    HWND        hWnd;
-    WNDCLASS    wc;
-    PIXELFORMATDESCRIPTOR pfd;
-    static HINSTANCE hInstance = 0;
+	WNDCLASSA wc;
+	HWND window;
+	HDC dc;
+	PIXELFORMATDESCRIPTOR pfd;
+	int pf;
 
-    /* only register the window class once - use hInstance as a flag. */
-    if (!hInstance) {
-	hInstance = GetModuleHandle(NULL);
+	memset(&wc, 0, sizeof(wc));
 	wc.style         = CS_OWNDC;
-	wc.lpfnWndProc   = (WNDPROC)WindowProc;
-	wc.cbClsExtra    = 0;
-	wc.cbWndExtra    = 0;
-	wc.hInstance     = hInstance;
-	wc.hIcon         = LoadIcon(NULL, IDI_WINLOGO);
+	wc.lpfnWndProc   = WindowProc;
+	wc.hInstance     = inst;
 	wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
-	wc.hbrBackground = NULL;
-	wc.lpszMenuName  = NULL;
-	wc.lpszClassName = "OpenGL";
+	wc.lpszClassName = "fcsim";
 
-	if (!RegisterClass(&wc)) {
-	    MessageBox(NULL, "RegisterClass() failed:  "
-		       "Cannot register window class.", "Error", MB_OK);
-	    return NULL;
-	}
-    }
+	RegisterClassA(&wc);
 
-    hWnd = CreateWindow("OpenGL", title, WS_OVERLAPPEDWINDOW |
+	window = CreateWindowA("fcsim", "fcsim", WS_OVERLAPPEDWINDOW |
 			WS_CLIPSIBLINGS | WS_CLIPCHILDREN,
-			x, y, width, height, NULL, NULL, hInstance, NULL);
+			0, 0, 256, 256, NULL, NULL, inst, NULL);
 
-    if (hWnd == NULL) {
-	MessageBox(NULL, "CreateWindow() failed:  Cannot create a window.",
-		   "Error", MB_OK);
-	return NULL;
-    }
+	dc = GetDC(window);
 
-    hDC = GetDC(hWnd);
+	memset(&pfd, 0, sizeof(pfd));
+	pfd.nSize = sizeof(pfd);
+	pfd.nVersion = 1;
+	pfd.dwFlags = PFD_SUPPORT_OPENGL | PFD_DRAW_TO_WINDOW | PFD_DOUBLEBUFFER;
+	pfd.iPixelType = PFD_TYPE_RGBA;
+	pfd.cColorBits = 32;
+	pfd.iLayerType = PFD_MAIN_PLANE;
 
-    /* there is no guarantee that the contents of the stack that become
-       the pfd are zeroed, therefore _make sure_ to clear these bits. */
-    memset(&pfd, 0, sizeof(pfd));
-    pfd.nSize        = sizeof(pfd);
-    pfd.nVersion     = 1;
-    pfd.dwFlags      = PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | flags;
-    pfd.iPixelType   = type;
-    pfd.cColorBits   = 32;
+	pf = ChoosePixelFormat(dc, &pfd);
 
-    pf = ChoosePixelFormat(hDC, &pfd);
-    if (pf == 0) {
-	MessageBox(NULL, "ChoosePixelFormat() failed:  "
-		   "Cannot find a suitable pixel format.", "Error", MB_OK); 
+	SetPixelFormat(dc, pf, &pfd);
+
+	HGLRC glrc = wglCreateContext(dc);
+	wglMakeCurrent(dc, glrc);
+
+	ShowWindow(window, cmd_show);
+
+	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
+	while (running) {
+		MSG msg;
+
+		glClear(GL_COLOR_BUFFER_BIT);
+		SwapBuffers(dc);
+		while (PeekMessageA(&msg, window, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
 	return 0;
-    } 
- 
-    if (SetPixelFormat(hDC, pf, &pfd) == FALSE) {
-	MessageBox(NULL, "SetPixelFormat() failed:  "
-		   "Cannot set format specified.", "Error", MB_OK);
-	return 0;
-    } 
-
-    DescribePixelFormat(hDC, pf, sizeof(PIXELFORMATDESCRIPTOR), &pfd);
-
-    ReleaseDC(hDC, hWnd);
-
-    return hWnd;
-}    
-
-int APIENTRY
-WinMain(HINSTANCE hCurrentInst, HINSTANCE hPreviousInst,
-	LPSTR lpszCmdLine, int nCmdShow)
-{
-    HDC hDC;				/* device context */
-    HGLRC hRC;				/* opengl context */
-    HWND  hWnd;				/* window */
-    MSG   msg;				/* message */
-
-    hWnd = CreateOpenGLWindow("minimal", 0, 0, 256, 256, PFD_TYPE_RGBA, 0);
-    if (hWnd == NULL)
-	exit(1);
-
-    hDC = GetDC(hWnd);
-    hRC = wglCreateContext(hDC);
-    wglMakeCurrent(hDC, hRC);
-
-    ShowWindow(hWnd, nCmdShow);
-
-    while(GetMessage(&msg, hWnd, 0, 0)) {
-	TranslateMessage(&msg);
-	DispatchMessage(&msg);
-    }
-
-    wglMakeCurrent(NULL, NULL);
-    ReleaseDC(hDC, hWnd);
-    wglDeleteContext(hRC);
-    DestroyWindow(hWnd);
-
-    return msg.wParam;
 }
-
