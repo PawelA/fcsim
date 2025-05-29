611fdf19c867f28089303720a6dbaa4d3b1fc9be
Paweł Anikiel <Pawel Anikiel pawel.anikiel@gmail.com>
2025-05-30 00:34:34 +0200

    enable new GL functions

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <windows.h>
-#include <GL/gl.h>
+#include "gl.h"
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -15,6 +15,75 @@ LRESULT CALLBACK WindowProc(HWND window, UINT msg, WPARAM wparam, LPARAM lparam)
 
 	return DefWindowProc(window, msg, wparam, lparam);
 } 
+
+void gl_load(void *(*load)(const char *));
+void *win32_load(const char *str);
+
+const GLchar *vertex_shader_src =
+	"attribute vec2 a_coords;"
+	"void main() {"
+		"gl_Position = vec4(a_coords, 0.0, 1.0);"
+	"}";
+
+const GLchar *fragment_shader_src =
+	"#ifdef GL_ES\n"
+	"precision mediump float;\n"
+	"#endif\n"
+	"void main() {"
+		"gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+	"}\n";
+
+static GLfloat coords[] = {
+	0.6, 0.6,
+	-0.6, -0.6,
+	0.0, 1.0,
+};
+
+GLuint program;
+GLuint coord_buffer;
+
+void gl_init(void)
+{
+	GLchar shader_log[1024];
+	GLuint vertex_shader;
+	GLuint fragment_shader;
+	GLint param;
+	GLsizei log_len;
+
+	vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &vertex_shader_src, NULL);
+	glCompileShader(vertex_shader);
+
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &param);
+	if (!param) {
+		glGetShaderInfoLog(vertex_shader, sizeof(shader_log), &log_len, shader_log);
+		printf("vertex shader:\n%s\n", shader_log);
+		exit(1);
+	}
+
+	fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment_shader, 1, &fragment_shader_src, NULL);
+	glCompileShader(fragment_shader);
+
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &param);
+	if (!param) {
+		glGetShaderInfoLog(fragment_shader, sizeof(shader_log), &log_len, shader_log);
+		printf("fragment shader:\n%s", shader_log);
+		exit(1);
+	}
+
+	program = glCreateProgram();
+	glAttachShader(program, vertex_shader);
+	glAttachShader(program, fragment_shader);
+	glLinkProgram(program);
+	glUseProgram(program);
+
+	glGenBuffers(1, &coord_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, coord_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(coords), coords, GL_STREAM_DRAW);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(0);
+}
 
 int APIENTRY WinMain(HINSTANCE inst, HINSTANCE prev_inst, LPSTR cmd_line, int cmd_show)
 {
@@ -56,12 +125,16 @@ int APIENTRY WinMain(HINSTANCE inst, HINSTANCE prev_inst, LPSTR cmd_line, int cm
 
 	ShowWindow(window, cmd_show);
 
-	glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+	gl_load(win32_load);
+	gl_init();
+	glClearColor(0.5, 0.3, 1.0, 1.0);
 
 	while (running) {
 		MSG msg;
 
 		glClear(GL_COLOR_BUFFER_BIT);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
 		SwapBuffers(dc);
 		while (PeekMessageA(&msg, window, 0, 0, PM_REMOVE)) {
 			TranslateMessage(&msg);
